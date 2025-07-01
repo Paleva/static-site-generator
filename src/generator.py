ca6794618805f6f2e13b5b9d535e03858f238ea4
Paleva <arnas.voitkevicius@gmail.com>
2025-07-01 12:48:50 +0300

    Fix not closing files after opening them.

--- src/generator.py
+++ src/generator.py
@@ -10,10 +10,15 @@ def extract_title(markdown: str):
 
 def generate_page(src_path: str, template_path: str, dst_path: str):
     print(f"Generating page from {src_path} to {dst_path} using {template_path}")
+    
     md_file = open(src_path, "r")
     template_file = open(template_path, "r")
     md_str = md_file.read()
     template_str = template_file.read()
+    
+    md_file.close()
+    template_file.close()
+    
     html_node = markdown_to_html_node(md_str)
     html_str = html_node.to_html()
     title = extract_title(md_str)
--- src/main.py
+++ src/main.py
@@ -1,11 +1,18 @@
 from textnode import TextNode
 from FSoperations import copy_files, delete_files
 from generator import generate_page
+import os.path
+
+CONTENT_DIR = "./content"
+STATIC_DIR = "./static"
+PUBLIC_DIR = "./public"
+TEMPLATE_DIR = "./template.html"
+
 
 def main():
-    delete_files('public')
-    copy_files('static', 'public')
-    generate_page("./content/index.md", "template.html", "public")
+    delete_files(PUBLIC_DIR)
+    copy_files(STATIC_DIR, PUBLIC_DIR)
+    generate_page(os.path.join(CONTENT_DIR, "index.md"), "template.html", PUBLIC_DIR)
 
 
 if __name__ == "__main__":
