3aacc1324b6fd1f20647fee8ef5d6fcdd46e76b2
Paleva <arnas.voitkevicius@gmail.com>
2025-07-01 11:15:31 +0300

    Start working on generating the action site files. Comment out debug statements in File system operation functions

---
+++ src/generator.py
@@ -0,0 +1,7 @@
+
+def extract_title(markdown: str):
+    pass
+
+
+def generate_page(src_path: str, template_path: str, dst_path: str):
+    print(f"Generating page from {src_path} to {dst_path} using {template_path}")
--- src/main.py
+++ src/main.py
@@ -1,50 +1,7 @@
 from textnode import TextNode
-import os
-import shutil
-from pathlib import Path
+from FSoperations import copy_files, delete_files
 
 
-def copy_files(src: str, dst: str):
-    if dir == None:
-        return None
-    cwd = Path.cwd()
-    src_full_path = os.path.join(cwd, src)
-    dst_full_path = os.path.join(cwd, dst)
-    
-    if not os.path.exists(dst_full_path):
-        os.mkdir(dst_full_path)
-    
-    dir_items = os.listdir(src_full_path)
-    for item in dir_items:
-        file_path = os.path.join(src_full_path, item)
-        if os.path.isdir(file_path):
-            dst_file_path = os.path.join(dst_full_path, item)
-            os.mkdir(dst_file_path)
-            copy_files(f"{src}/{item}", f"{dst}/{item}")
-            print(f"CREATED DIRECTORY: {dst_full_path}/{item}")
-        elif os.path.isfile(file_path):
-            shutil.copy(f"{file_path}", f"{dst_full_path}")
-            print(f"COPIED FILE: {file_path} -> {src_full_path}")
-
-def delete_files(dir: str = None):
-    if dir == None:
-        return None
-    cwd = Path.cwd()
-    full_path = os.path.join(cwd, dir)
-
-    if not os.path.exists(full_path):
-        return None
-
-    dir_items = os.listdir(full_path)
-    for item in dir_items:
-        file_path = os.path.join(full_path, item)
-        if os.path.isdir(file_path):
-            delete_files(f"{dir}/{item}")
-            os.rmdir(file_path)
-            print(f"DELETED DIRECTORY: {file_path}")
-        elif os.path.isfile(file_path):
-            os.remove(file_path)
-            print(f"DELETED FILE: {file_path}")
 def main():
     # node = TextNode("Hello, World!", "plain", None)
     # print(node)
